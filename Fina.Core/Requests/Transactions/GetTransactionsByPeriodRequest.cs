6dee0f58452e34bcbd765c540446c68a79e06c56
gwcruzara <gwcruzara@gmail.com>
2024-06-19 20:54:41 -0300

    build: First commit

---
+++ Fina.Core/Requests/Transactions/GetTransactionsByPeriodRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Fina.Core.Requests.Transactions
+{
+    public class GetTransactionsByPeriodRequest : Request
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
